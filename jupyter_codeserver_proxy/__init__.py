95ba800fce36f86387ee2b2d950e6f51304e8c28
ddl-aambekar <55107387+ddl-aambekar@users.noreply.github.com>
2020-03-10 19:32:31 -0700

    Update __init__.py

--- jupyter_codeserver_proxy/__init__.py
+++ jupyter_codeserver_proxy/__init__.py
@@ -17,7 +17,7 @@ def setup_codeserver():
         if working_dir is None:
             working_dir = os.getenv("JUPYTER_SERVER_ROOT", ".")
 
-        return ['--port=' + str(port), full_path, working_dir, "--auth none", " --disable-telemetry"]
+        return [full_path, working_dir, '--port=' + str(port), "--auth=none", " --disable-telemetry"]
         
     return {
         'command': _codeserver_command,
